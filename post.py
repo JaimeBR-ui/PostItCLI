4a2086f45a0b7e32b370f74f0771155467197f48
JaimeBR-ui <bambas@knights.ucf.edu>
2020-03-11 05:20:18 -0400

    Changed show method parameter to text instead of index.

--- post.py
+++ post.py
@@ -2,6 +2,7 @@
 # Filename: post.py
 
 import sys
+from note import *
 from menu import print_preview
 
 # Let's get how many command line args we have.
@@ -13,9 +14,19 @@ length = len(sys.argv)
 if length == 1:
     print_preview()
 
-elif length > 1 and sys.argv[1] == "it":
+elif length > 2 and sys.argv[1] == "it":
     # Let's save the rest of the arguments
-    pass
+    note = to_string(sys.argv, 2)
+    write(note)
+
 elif length > 1 and sys.argv[1] == "show":
     # Show the contents
-    pass
+    if length > 2:
+        note = to_string(sys.argv, 2)
+        read(note)
+    else:
+        # Ouput everything to terminal.
+        read(None)
+
+elif length > 1 and sys.argv[1] == "wipe":
+    wipe()
